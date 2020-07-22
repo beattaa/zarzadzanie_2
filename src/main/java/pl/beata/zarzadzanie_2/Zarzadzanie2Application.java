28d0f568470678840e18533b3770339f986755e5
beata <beata_sadek@o2.pl>
2020-07-22 18:32:00 +0200

    first_commit

---
+++ src/main/java/pl/beata/zarzadzanie_2/Zarzadzanie2Application.java
@@ -0,0 +1,13 @@
+package pl.beata.zarzadzanie_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zarzadzanie2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zarzadzanie2Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/beata/zarzadzanie_2/Zarzadzanie2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.beata.zarzadzanie_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zarzadzanie2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
